5e986ff8136760f5559b6774961819dd8730ba34
JamilDroubi <jawsomeone@hotmail.com>
2020-08-26 21:00:46 -0700

    Minor tweaks

--- config.h
+++ config.h
@@ -33,13 +33,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,      },
-	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Surf",     NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Tabbed Surf",     NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Tabbed",     NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Emacs",    NULL,       NULL,       1 << 2,       0,           -1 },
+	/* class          instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",         NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",      NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Surf",         NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Tabbed Surf",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Tabbed",       NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Emacs",        NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
 /* layout(s) */
